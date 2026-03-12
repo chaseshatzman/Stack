081f12eb6452c5abf1bf1438b15b8f50ede0ec04
dfos_hari <dfos_hari@FXMACs-MacBook-Air.local>
2026-03-12 02:04:05 -0400

    created another block and made it move back and forth

--- stack_game.m
+++ stack_game.m
@@ -10,6 +10,12 @@ blockHeight = 1;
 blockWidth = 3;
 %block size
 
+currentXpos = 0;
+currentYpos = 12;
+currentHeight = 1;
+currentWidth = 3;
+direction = 0.1;
+
 figure('Color', 'k', 'Name', 'Stack Game', 'NumberTitle', 'off')
 
 axis([0 gameWidth 0 gameHeight])
@@ -23,4 +29,36 @@ set(gca, 'Color', 'k')
 
 disableDefaultInteractivity(gca)
 
-rectangle('Position', [blockXpos blockYpos blockWidth blockHeight], 'FaceColor', 'b', 'EdgeColor', 'b')
+%bottomBlock = rectangle('Position', [blockXpos blockYpos blockWidth blockHeight], 'FaceColor', 'b', 'EdgeColor', 'w')
+%topBlock = rectangle('Position', [currentXpos currentYpos currentWidth blockHeight], 'FaceColor', 'b', 'EdgeColor', 'w')
+
+% Initialize the game loop
+while true
+    
+    % Clear the current rectangle before redrawing
+     cla;
+
+     axis([0 gameWidth 0 gameHeight])
+     axis equal
+     hold on
+     box on
+     set(gca, 'LineWidth', 2, 'XColor', 'b', 'YColor', 'b', 'XTick', [], 'YTick', [])
+     set(gca,'Color', 'k')
+     disableDefaultInteractivity(gca)
+
+    
+    % Redraw the block and current position
+    rectangle('Position', [blockXpos blockYpos blockWidth blockHeight], 'FaceColor', 'b', 'EdgeColor', 'w');
+    rectangle('Position', [currentXpos currentYpos currentWidth currentHeight], 'FaceColor', 'b', 'EdgeColor', 'w');
+   
+    currentXpos = currentXpos +direction;
+    
+    % Update the current position based on the direction
+    if currentXpos <= 0 ||  currentXpos + direction >= gameWidth
+        direction = -direction;
+    end
+
+    %set(topBlock, 'Position', [currentXpos currentYpos currentWidth currentHeight])
+
+    pause(0.02); % Control the speed of the loop
+end 
